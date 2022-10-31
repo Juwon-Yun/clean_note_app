45f258fa1542f6d3fe7ec254e0875eaa72b4440b
Juwon-Yun <eooll2@daum.net>
2022-10-31 21:35:19 +0900

    - add: add, edit note event 정의

---
+++ lib/presentation/add_edit_note/add_edit_note_view_model.dart
@@ -0,0 +1,48 @@
+import 'package:clean_note/domain/model/note.dart';
+import 'package:clean_note/domain/repository/note_repository.dart';
+import 'package:clean_note/presentation/add_edit_note/add_edit_note_event.dart';
+import 'package:flutter/material.dart';
+
+class AddEditNoteViewModel with ChangeNotifier {
+  final NoteRepository repository;
+
+  int _color = Colors.orange.value;
+  int get color => _color;
+
+  AddEditNoteViewModel(this.repository);
+
+  void onEvent(AddEditNoteEvent event) {
+    event.when(
+      changeColor: _changeColor,
+      saveNote: _saveNote,
+    );
+  }
+
+  Future<void> _changeColor(int color) async {
+    _color = color;
+    notifyListeners();
+  }
+
+  Future<void> _saveNote(int? id, String title, String content) async {
+    if (id == null) {
+      // insert
+      await repository.insertNote(
+        Note(
+            title: title,
+            content: content,
+            color: _color,
+            timestamp: DateTime.now().millisecondsSinceEpoch),
+      );
+    } else {
+      // update
+      await repository.updateNote(
+        Note(
+            id: id,
+            title: title,
+            content: content,
+            color: color,
+            timestamp: DateTime.now().millisecondsSinceEpoch),
+      );
+    }
+  }
+}
