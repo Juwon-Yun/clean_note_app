edad08c24078040874d26879669fa52cdbf449ab
Juwon-Yun <eooll2@daum.net>
2022-11-05 17:02:38 +0900

    - update: ui 수정

--- lib/presentation/notes/components/note_item.dart
+++ lib/presentation/notes/components/note_item.dart
@@ -8,18 +8,34 @@ class NoteItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(8.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: Color(note.color),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(note.title),
-          Text(note.content),
-        ],
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Container(
+        padding: const EdgeInsets.all(14),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          color: Color(note.color),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              note.title,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: Theme.of(context).textTheme.headline5,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              note.content,
+              maxLines: 10,
+              overflow: TextOverflow.ellipsis,
+              style: Theme.of(context).textTheme.bodyText2,
+            ),
+          ],
+        ),
       ),
     );
   }
