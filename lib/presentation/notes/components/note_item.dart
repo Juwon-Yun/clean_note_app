965ff7eb4815b6fbe7d0dcff4536ca4e957f844b
Juwon-Yun <eooll2@daum.net>
2022-11-05 21:11:37 +0900

    - update: reorderable noteitem

--- lib/presentation/notes/components/note_item.dart
+++ lib/presentation/notes/components/note_item.dart
@@ -3,39 +3,62 @@ import 'package:flutter/material.dart';
 
 class NoteItem extends StatelessWidget {
   final Note note;
+  final VoidCallback onDeleteTap;
 
-  const NoteItem({Key? key, required this.note}) : super(key: key);
+  const NoteItem({
+    Key? key,
+    required this.note,
+    required this.onDeleteTap,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8),
-      child: Container(
-        padding: const EdgeInsets.all(14),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12),
-          color: Color(note.color),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              note.title,
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-              style: Theme.of(context).textTheme.headline5,
+      child: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.all(14),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: Color(note.color),
             ),
-            const SizedBox(
-              height: 10,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  note.title,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: Theme.of(context).textTheme.headline5,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  note.content,
+                  maxLines: 10,
+                  overflow: TextOverflow.ellipsis,
+                  style: Theme.of(context).textTheme.bodyText2,
+                ),
+              ],
             ),
-            Text(
-              note.content,
-              maxLines: 10,
-              overflow: TextOverflow.ellipsis,
-              style: Theme.of(context).textTheme.bodyText2,
+          ),
+          Positioned(
+            bottom: 8,
+            right: 8,
+            child: GestureDetector(
+              onTap: () {
+                onDeleteTap.call();
+              },
+              child: const Icon(
+                Icons.delete,
+                color: Color(0xFF424242),
+              ),
             ),
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
--- lib/presentation/notes/notes_screen.dart
+++ lib/presentation/notes/notes_screen.dart
@@ -3,9 +3,15 @@ import 'package:clean_note/presentation/notes/components/note_item.dart';
 import 'package:clean_note/ui/colors.dart';
 import 'package:flutter/material.dart';
 
-class NoteScreen extends StatelessWidget {
+class NoteScreen extends StatefulWidget {
   const NoteScreen({Key? key}) : super(key: key);
 
+  @override
+  State<NoteScreen> createState() => _NoteScreenState();
+}
+
+class _NoteScreenState extends State<NoteScreen> {
+  final List<int> _items = List<int>.generate(50, (int index) => index);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,27 +38,57 @@ class NoteScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: ListView(
-          children: [
-            NoteItem(
-              note: Note(
-                title: 'title',
-                content: 'content',
-                color: wisteria.value,
-                timestamp: DateTime.now().millisecondsSinceEpoch,
-              ),
-            ),
-            NoteItem(
-              note: Note(
-                title: 'title',
-                content: 'content',
-                color: skyBlue.value,
-                timestamp: DateTime.now().millisecondsSinceEpoch,
-              ),
-            ),
-          ],
+        child: ReorderableListView.builder(
+          onReorder: (int oldIndex, int newIndex) {
+            if (oldIndex < newIndex) {
+              newIndex -= 1;
+            }
+
+            final item = _items.removeAt(oldIndex);
+            _items.insert(newIndex, item);
+          },
+          itemBuilder: (context, index) {
+            return renderNoteItem(index: index);
+          },
+          itemCount: 2,
         ),
       ),
     );
   }
+
+  NoteItem renderNoteItem({required int index}) {
+    return NoteItem(
+      key: Key(index.toString()),
+      onDeleteTap: () {},
+      note: Note(
+        title: 'title ${_items[index]}',
+        content: 'content ${_items[index]}',
+        color: _items[index].isEven ? wisteria.value : skyBlue.value,
+        timestamp: DateTime.now().millisecondsSinceEpoch,
+      ),
+    );
+  }
+}
+
+List<NoteItem> noteItems() {
+  return [
+    NoteItem(
+      onDeleteTap: () {},
+      note: Note(
+        title: 'title 1',
+        content: 'content 1',
+        color: wisteria.value,
+        timestamp: DateTime.now().millisecondsSinceEpoch,
+      ),
+    ),
+    NoteItem(
+      onDeleteTap: () {},
+      note: Note(
+        title: 'title 2',
+        content: 'content 2',
+        color: skyBlue.value,
+        timestamp: DateTime.now().millisecondsSinceEpoch,
+      ),
+    ),
+  ];
 }
