707288aecb3e6d0b35cd55a067cb94bba844e40d
Juwon-Yun <eooll2@daum.net>
2022-11-08 21:56:52 +0900

    - update: title, content textEditingController

--- lib/presentation/add_edit_note/add_edit_note_screen.dart
+++ lib/presentation/add_edit_note/add_edit_note_screen.dart
@@ -1,8 +1,31 @@
 import 'package:clean_note/ui/colors.dart';
 import 'package:flutter/material.dart';
 
-class AddEditNoteScreen extends StatelessWidget {
+class AddEditNoteScreen extends StatefulWidget {
   const AddEditNoteScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AddEditNoteScreen> createState() => _AddEditNoteScreenState();
+}
+
+class _AddEditNoteScreenState extends State<AddEditNoteScreen> {
+  final _titleController = TextEditingController();
+  final _contentController = TextEditingController();
+
+  final List<Color> noteColor = [
+    roseBud,
+    primrose,
+    wisteria,
+    skyBlue,
+    illusion,
+  ];
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _contentController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,8 +42,11 @@ class AddEditNoteScreen extends StatelessWidget {
         color: Colors.grey,
         child: Column(
           children: [
-            Row(),
+            Row(
+              children: [],
+            ),
             TextField(
+              controller: _titleController,
               maxLines: 1,
               style: Theme.of(context)
                   .textTheme
@@ -32,6 +58,7 @@ class AddEditNoteScreen extends StatelessWidget {
               ),
             ),
             TextField(
+              controller: _contentController,
               maxLines: null,
               style: Theme.of(context)
                   .textTheme
