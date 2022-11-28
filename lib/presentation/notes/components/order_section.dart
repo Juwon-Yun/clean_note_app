aeb2f79b89b3440534d2f9eaf973fa8e93a37d28
Juwon-Yun <eooll2@daum.net>
2022-11-28 22:13:06 +0900

    - add: Radio NoteOrder Type

--- lib/presentation/notes/components/order_section.dart
+++ lib/presentation/notes/components/order_section.dart
@@ -1,10 +1,37 @@
+import 'package:clean_note/domain/util/note_order.dart';
 import 'package:flutter/material.dart';
 
+typedef OnOrderChange = Function(NoteOrder noteOrder);
+
 class OrderSection extends StatelessWidget {
-  const OrderSection({Key? key}) : super(key: key);
+  final NoteOrder noteOrder;
+  final OnOrderChange onOrderChange;
+  const OrderSection({
+    Key? key,
+    required this.onOrderChange,
+    required this.noteOrder,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        Row(
+          children: [
+            Radio<NoteOrder>(
+                value: NoteOrder.title(noteOrder.orderType),
+                groupValue: noteOrder,
+                onChanged: (NoteOrder? value) {
+                  onOrderChange(
+                    NoteOrder.title(noteOrder.orderType),
+                  );
+                })
+          ],
+        ),
+        Row(
+          children: [],
+        ),
+      ],
+    );
   }
 }
